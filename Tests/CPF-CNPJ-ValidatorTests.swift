558112030a7b4c85d230afb25c52f6454f997e08
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-10-23 03:44:05 -0200

    Add some CPF tests

--- Tests/CPF-CNPJ-ValidatorTests.swift
+++ Tests/CPF-CNPJ-ValidatorTests.swift
@@ -2,10 +2,78 @@ import XCTest
 @testable import CPF_CNPJ_Validator
 
 class CPF_CNPJ_ValidatorTests: XCTestCase {
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    let validator = Validator()
+
+    func testEmptyCPF() {
+        let cpf = ""
+        let status = validator.validate(cpf: cpf)
+        XCTAssertEqual(status, Status.wrongLength)
     }
-    
+
+    func testEmptyCPFAddingLeadingZeros() {
+        let cpf = ""
+        let status = validator.validate(cpf: cpf, options: [.addLeadingZeros])
+        XCTAssertEqual(status, Status.repeatedPattern)
+    }
+
+    func testEmptyCPFAddingLeadingAllowingRepeatedPatterns() {
+        let cpf = ""
+        let status = validator.validate(cpf: cpf, options: [.addLeadingZeros, .allowRepeatedPatterns])
+        XCTAssertEqual(status, Status.valid)
+    }
+
+    func testShortCPF() {
+        let cpf = "123"
+        let status = validator.validate(cpf: cpf)
+        XCTAssertEqual(status, Status.wrongLength)
+    }
+
+    func testLongCPF() {
+        let cpf = "1234567890912345678909"
+        let status = validator.validate(cpf: cpf)
+        XCTAssertEqual(status, Status.wrongLength)
+    }
+
+    func testLongCPFIgnoringRemainingCharacters() {
+        let cpf = "1234567890912345678909"
+        let status = validator.validate(cpf: cpf, options: [.ignoreRemainingCharacters])
+        XCTAssertEqual(status, Status.commonNumber)
+    }
+
+    func testLongCPFIgnoringRemainingCharactersAllowingCommonNumbers() {
+        let cpf = "1234567890912345678909"
+        let status = validator.validate(cpf: cpf, options: [.ignoreRemainingCharacters, .allowCommonNumbers])
+        XCTAssertEqual(status, Status.valid)
+    }
+
+    func testInvalidCPF() {
+        let cpf = "01002003040"
+        let status = validator.validate(cpf: cpf)
+        XCTAssertEqual(status, Status.invalid)
+    }
+
+    func testValidCPF() {
+        let cpf = "01002003032"
+        let status = validator.validate(cpf: cpf)
+        XCTAssertEqual(status, Status.valid)
+    }
+
+    func testValidCPFWithLettersMixed() {
+        let cpf = "01002aaaa003032"
+        let status = validator.validate(cpf: cpf)
+        XCTAssertEqual(status, Status.invalid)
+    }
+
+    func testValidCPFWithLetterMixedInterpretingOnlyNumbers() {
+        let cpf = "01002aaaa003032"
+        let status = validator.validate(cpf: cpf, options: [.interpretOnlyNumbers])
+        XCTAssertEqual(status, Status.valid)
+    }
+
+    func testValidFormattedCPF() {
+        let cpf = "010.020.030-32"
+        let status = validator.validate(cpf: cpf)
+        XCTAssertEqual(status, Status.valid)
+    }
 }
