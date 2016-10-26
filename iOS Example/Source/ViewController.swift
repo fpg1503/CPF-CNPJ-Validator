5af4553a16407691a36f159c962365aaa5288e2f
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-10-26 01:35:10 -0200

    Add simple example

--- iOS Example/Source/ViewController.swift
+++ iOS Example/Source/ViewController.swift
@@ -1,4 +1,20 @@
 import UIKit
+import CPF_CNPJ_Validator
 
-class ViewController: UIViewController { }
+class ViewController: UIViewController {
+    @IBOutlet fileprivate var textField: UITextField?
+    @IBOutlet fileprivate var label: UILabel?
 
+    var text = "" {
+        didSet {
+            let success = BooleanValidator().validate(cpf: text)
+            label?.text = success ? "CPF Válido" : "CPF Inválido"
+        }
+    }
+}
+
+extension ViewController {
+    @IBAction func didChange() {
+        text = textField?.text ?? ""
+    }
+}
